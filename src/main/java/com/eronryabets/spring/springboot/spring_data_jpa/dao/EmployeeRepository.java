298f8ac5c7e83d2024d044464c47ea6482925d5b
Ihor Ryabets <eronryabets@gmail.com>
2022-02-24 00:06:22 +0200

    JpaRepository

---
+++ src/main/java/com/eronryabets/spring/springboot/spring_data_jpa/dao/EmployeeRepository.java
@@ -0,0 +1,13 @@
+package com.eronryabets.spring.springboot.spring_data_jpa.dao;
+
+
+import com.eronryabets.spring.springboot.spring_data_jpa.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    public List<Employee> findAllByName(String name);
+
+}
